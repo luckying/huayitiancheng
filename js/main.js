21a875c94e3dd595a7db5da76bf2f620ca126ae1
luckying <wangminglucky@sina.com>
2017-04-23 17:33:25 +0800

    demo

--- js/main.js
+++ js/main.js
@@ -38,21 +38,36 @@ $(document).ready(function(){
     
     /*下拉菜单*/
     var nav=$("#header .h-nav .nav"),
-        menu=nav.find(".menu");
+        menu=nav.find(".menu"),
+        bSys=false;
+    nav.each(function(){
+        $(this).mouseenter(function(){
+            if(!bSys){
+                bSys=!bSys;
+                $(this).addClass("on").find(".menu").slideDown(600);
+                setTimeout(function(){
+                    bSys=!bSys;
+                },100)
+            }
+        })
+        $(this).mouseleave(function(){
+            $(this).removeClass("on").find(".menu").hide();
+        })
 
-    nav.on("mouseover",function(){
-    	var me=$(this);
-    	var index=me.index();
-    	me.addClass('on');
-    	menu.eq(index).slideDown('slow');
     })
+    // nav.on("mouseover",function(){
+    // 	var me=$(this);
+    // 	var index=me.index();
+    // 	me.addClass('on');
+    // 	menu.eq(index).slideDown('slow');
+    // })
 
-    nav.on("mouseleave",function(){
-    	var me=$(this);
-    	var index=me.index();
-    	me.removeClass("on");
-    	menu.eq(index).slideUp()
-    }) 
+    // nav.on("mouseleave",function(){
+    // 	var me=$(this);
+    // 	var index=me.index();
+    // 	me.removeClass("on");
+    // 	menu.eq(index).slideUp()
+    // }) 
 
 
 })
